284bc8187c1485085050662d8e5ce268e9a33d03
Diana Migasiuk <diana.migasiuk@contractors.roche.com>
2017-06-20 12:27:04 +0200

    init

---
+++ src/main/java/pl/kobietydokodu/bilet/bilet/BiletApplication.java
@@ -0,0 +1,12 @@
+package pl.kobietydokodu.bilet.bilet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiletApplication.class, args);
+	}
+}
